c10938db98ac04f956203605f79dac913ba5515f
fxz <39674855+Iamfxz@users.noreply.github.com>
2022-03-11 20:48:12 +0800

    edd rule debug

--- loose.py
+++ loose.py
@@ -8,100 +8,133 @@
 @desc:
     创建者请写入文件说明
 """
+import copy
 import operator
 import sys
 from itertools import permutations
 from typing import List
 
+cnt = 0
+lower_bound = sys.maxsize  # 可中断情况下的下界 1 | r_{j},prmp | min(L_{max})
+upper_bound = sys.maxsize  # 可行解的结果来更新
+
 
 class TreeNode:
-    def __init__(self, tasks, seq, curr=0, low_bound=sys.maxsize):
+    def __init__(self, tasks, seq, curr=0):
         self.tasks = tasks  # 所有的任务
-        self.seq = seq  # 已经确定的路径
-        self.remain_tasks = [t for t in tasks if t[0] not in seq]  # 未完成的任务
-        self.seq_tasks = []  # 已经完成的任务
-        for i in seq:
-            for j in tasks:
-                if j[0] == i:
-                    self.seq_tasks.append(j)
-        self.curr = curr  # 当前运行到的时间
-        self.low_bound = low_bound  # 可中断情况下的下界 1 | r_{j},prmp | min(L_{max})
-        self.obj_val = sys.maxsize  # search the min of Lmax
+        self.task_dict = dict()
+        for i in tasks:
+            self.task_dict[i[0]] = i
+        self.seq = []  # 已经确定的路径
+        self.curr = 0  # 当前运行到的时间
+        self.set_seq(seq)
+        self.remain_tasks = [t for t in tasks if t[0] not in self.seq]  # 未完成的任务
+        self.obj_val = self.get_obj_val()  # search the min of Lmax
         self.child = []  # 所有的子节点
+
+    def set_seq(self, seq):
+        self.seq = seq
+        curr = 0
+        for s in self.seq:
+            t = self.task_dict[s]
+            curr = max(curr, t[1]) + t[2]
+        self.curr = curr
 
     def add_child(self, node):
         self.child.append(node)
         return self
 
-    def get_obj_val(self):
+    def get_obj_val(self, seq=None):
+        if seq is None:
+            seq = self.seq
         # 计算已经排程部分的目标值
-        if len(self.seq) == 0:
-            return sys.maxsize
+        if len(seq) == 0:
+            return 0
         release = []
         process = []
         due = []
-        for j in self.seq_tasks:
-            release.append(j[1])
-            process.append(j[2])
-            due.append(j[3])
+        for s in seq:
+            t = self.task_dict[s]
+            release.append(t[1])
+            process.append(t[2])
+            due.append(t[3])
         finish = [0] * len(release)
-        if len(self.seq) == 1:
+        if len(seq) == 1:
             late = release[0] + process[0] - due[0]
         else:
-            for i in range(len(self.seq)):
+            for i in range(len(seq)):
                 finish[i] = max(finish[i - 1], release[i]) + process[i]
             late = max([f - d for d, f in zip(due, finish)])
-        late = max(0, late)
+        # late = max(0, late)
         return late
 
     def compute(self):
         # 计算未排程部分
-        temp_seq = self.seq
+        temp_seq = copy.copy(self.seq)
         temp_curr = self.curr
         task_num = len(self.remain_tasks)  # 剩余task数量
-        edd_tasks = sorted(self.remain_tasks, key=operator.itemgetter(3))  # EDD rule
-
+        edd_tasks = copy.deepcopy(sorted(self.remain_tasks, key=operator.itemgetter(3)))  # EDD rule
+        lateness_time = [self.get_obj_val()]
         while len(temp_seq) < task_num:
-            unscheduled_tasks = edd_tasks  # unscheduled task
+            unscheduled_tasks = [i for i in edd_tasks if i[0] not in temp_seq]  # unscheduled task
             if unscheduled_tasks[0][1] <= temp_curr:  # earliest due date of task's release time is achieve
                 # directly compute the first task
                 temp_seq.append(unscheduled_tasks[0][0])
                 temp_curr = max(unscheduled_tasks[0][1], temp_curr)
                 temp_curr += unscheduled_tasks[0][2]
+                lateness_time.append(temp_curr - unscheduled_tasks[0][3])
             else:
-                # compute the part/whole min release time of task
-                min_release_task1 = sorted(unscheduled_tasks, key=operator.itemgetter(1))[0]
-                ix = unscheduled_tasks.index(min_release_task1)
-                temp_curr = max(temp_curr, min_release_task1[1])  # 结合release time获取此时的开始时间
-                if ix == 0:
-                    # EDD第一单就是release time最小，直接加工
-                    temp_seq.append(min_release_task1[0])
-                    temp_curr += min_release_task1[2]
-                else:
-                    # 比最小release time的due date还小的里面，找到最小release time
-                    min_release_task2 = sorted(unscheduled_tasks[:ix], key=operator.itemgetter(1))[0]
-                    available_process_time = (
-                        min_release_task2[1] - min_release_task1[1]
-                    )  # 两个release time的差即为最小release time task需要加工的时间
-                    if available_process_time < min_release_task1[2]:
-                        # compute part of min release time of task
-                        temp_curr += available_process_time
-                        min_release_task1[2] -= available_process_time  # 该task消耗部分加工时间
-                    else:
-                        # compute whole of min release time of task
-                        temp_seq.append(min_release_task1[0])
-                        temp_curr += min_release_task1[2]
-        print(self.seq, temp_seq, temp_curr)
+                need_time = unscheduled_tasks[0][1] - temp_curr
+                # 尝试从后面找时间填充它
+                while need_time > 0:
+                    no_change = 0
+                    for j in unscheduled_tasks[1:]:
+                        # 找到可以填充的工单
+                        if j[1] <= temp_curr:
+                            if j[2] > need_time:  # compute part of min release time of task
+                                temp_curr += need_time
+                                for t in range(len(edd_tasks)):
+                                    if edd_tasks[t][0] == j[0]:
+                                        edd_tasks[t][2] -= need_time
+                                need_time = 0
+                            else:  # compute whole of min release time of task
+                                temp_seq.append(j[0])
+                                temp_curr += j[2]
+                                lateness_time.append(temp_curr - j[3])
+                                need_time -= j[2]
+                        # 找不到可以填充的工单
+                        else:
+                            no_change += 1
+                    if no_change == len(unscheduled_tasks[1:]):
+                        # 没有办法填充，只能idle
+                        temp_curr += need_time
+                        need_time = 0
+        # global cnt
+        # cnt += 1
+        # print(cnt, ":", self.seq, temp_seq, temp_curr)
+        lb = max(lateness_time)
+        return lb  # 下界
         # self.low_bound = temp_curr
 
 
 def traversal(root: TreeNode) -> TreeNode:
     all_tasks = root.tasks
+    # 直接构造可行解作为，上界
+    all_tasks = sorted(all_tasks, key=operator.itemgetter(3))  # EDD sorted
+    feasible_node = copy.deepcopy(root)
+    seq = [t[0] for t in all_tasks]
+    feasible_node.set_seq(seq)
+    global upper_bound
+    upper_bound = feasible_node.get_obj_val()
+    print("feasible solution(upper bound):", upper_bound)
 
     def dfs(curr_node):
         curr_node.compute()
         if len(curr_node.remain_tasks) == 0:
-            print(curr_node.get_obj_val())
+            # 产生可行解
+            print(
+                f"feasible solution:{curr_node.get_obj_val()}, LB:{curr_node.compute()}, UB:{upper_bound},seq:{curr_node.seq}"
+            )
             return
         tasks = [t for t in curr_node.tasks if t[0] not in curr_node.seq]
         for t in tasks:
@@ -110,6 +143,9 @@ def traversal(root: TreeNode) -> TreeNode:
             child_curr = max(curr_node.curr, t[1]) + t[2]
             child_node = TreeNode(all_tasks, child_seq, child_curr)
             curr_node.add_child(child_node)
+            print(
+                f"obj value:{curr_node.get_obj_val()}, LB:{curr_node.compute()}, UB:{upper_bound},seq:{curr_node.seq}"
+            )
             dfs(child_node)
 
     dfs(root)
@@ -163,7 +199,7 @@ def deep_first_search():
     m.output()
     root = TreeNode(m.tasks, [])
     traversal(root)
-    print("low bound:", root.low_bound)
+    print("low bound:")
     print("seq of low bound:", root.seq)
 
 
@@ -172,7 +208,7 @@ def EDD_rule():
     m.output()
     root = TreeNode(m.tasks, [])
     root.compute()
-    print("low bound:", root.low_bound)
+    print("low bound:")
     print("seq of low bound:", root.seq)
     assert root.low_bound == 17
 
